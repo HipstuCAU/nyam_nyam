e62faece17c82fee64e62fde2a01d58769b6069e
feldblume5263 <feldblume5263@gmail.com>
2023-02-27 18:47:55 +0900

    update project running without storyboard

--- NyamNyam/NyamNyam/Screens/Home/HomeViewController.swift
+++ NyamNyam/NyamNyam/Screens/Home/HomeViewController.swift
@@ -6,13 +6,24 @@
 //
 
 import UIKit
+import SnapKit
 
 class HomeViewController: UIViewController {
     let viewModel = HomeViewModel()
 
+    let testLabel = UILabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        view.addSubview(testLabel)
+        testLabel.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
+        testLabel.text = "힙스터들 환영해!!"
+        testLabel.textColor = .blue
+        testLabel.textAlignment = .center
+        testLabel.font = .systemFont(ofSize: 30)
     }
 
 
