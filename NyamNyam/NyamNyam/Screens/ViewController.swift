425c29ebf35230d7d1c0ad3b22f52fde763d3170
feldblume5263 <feldblume5263@gmail.com>
2023-02-27 17:21:59 +0900

    init project

---
+++ NyamNyam/NyamNyam/Screens/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NyamNyam
+//
+//  Created by Noah Park on 2023/02/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
