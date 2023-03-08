7bebf38ee6b058178deb4a64c5742b35f81f3c40
feldblume5263 <feldblume5263@gmail.com>
2023-03-08 23:48:46 +0900

    feat: #19 collectionview 프레임 설정

---
+++ NyamNyam/NyamNyam/Screens/Home/ContentCarouselModule/ContentCarouselModuleViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ContentCarouselModuleViewController.swift
+//  NyamNyam
+//
+//  Created by Noah Park on 2023/03/08.
+//
+
+import UIKit
+
+final class ContentCarouselModuleViewController: UIViewController {
+
+    let viewModel: HomeViewModel
+    
+    init(viewModel: HomeViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+}
+
+
+// MARK: DataSource
+extension ContentCarouselModuleViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+    
+  
+}
--- NyamNyam/NyamNyam/Screens/Home/HomeViewController.swift
+++ NyamNyam/NyamNyam/Screens/Home/HomeViewController.swift
@@ -10,7 +10,8 @@ import SnapKit
 
 final class HomeViewController: UIViewController {
     let viewModel = HomeViewModel()
-    lazy var OptionSelectModule = OptionSelectModuleViewController(viewModel: viewModel)
+    lazy var optionSelectModule = OptionSelectModuleViewController(viewModel: viewModel)
+    lazy var contentCarouselModule = ContentCarouselModuleViewController(viewModel: viewModel)
     
     override var preferredStatusBarStyle: UIStatusBarStyle {
         if #available(iOS 13, *) {
@@ -25,19 +26,29 @@ final class HomeViewController: UIViewController {
         navigationController?.setNavigationBarHidden(true, animated: true)
         view.backgroundColor = .white
         setOptionSelectModuleLayout()
+        setContentCarouselModuleLayout()
     }
 }
 
 extension HomeViewController {
-    
+    private func setContentCarouselModuleLayout() {
+        self.addChild(contentCarouselModule)
+        self.view.addSubview(contentCarouselModule.view)
+        contentCarouselModule.didMove(toParent: self)
+        contentCarouselModule.view.snp.makeConstraints { make in
+            make.top.equalTo(optionSelectModule.view
+                .snp.bottom)
+            make.leading.trailing.bottom.equalToSuperview()
+        }
+    }
 }
 
 extension HomeViewController {
     private func setOptionSelectModuleLayout() {
-        self.addChild(OptionSelectModule)
-        self.view.addSubview(OptionSelectModule.view)
-        OptionSelectModule.didMove(toParent: self)
-        OptionSelectModule.view.snp.makeConstraints { make in
+        self.addChild(optionSelectModule)
+        self.view.addSubview(optionSelectModule.view)
+        optionSelectModule.didMove(toParent: self)
+        optionSelectModule.view.snp.makeConstraints { make in
             make.top.equalTo(view.snp.top).offset(63)
             make.leading.equalToSuperview()
             make.trailing.equalToSuperview()
