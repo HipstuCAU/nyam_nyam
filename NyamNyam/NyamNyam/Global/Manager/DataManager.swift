c12c2ad8c1298b0db6a0b85e32e588d90ebff9c9
Taekhwan Han <hth3396@gmail.com>
2023-03-09 14:25:17 +0900

    feat: #15 Meal 모델에 맞추어 데이터를 파싱하는 함수 구현 cafeteria, mealType, mealTime, menu, price 를 Meal 모델에 맞추어 파싱하도록 합니다.

--- NyamNyam/NyamNyam/Global/Manager/DataManager.swift
+++ NyamNyam/NyamNyam/Global/Manager/DataManager.swift
@@ -21,5 +21,53 @@ final class DataManager {
 }
 
 private extension DataManager {
+    func getMealTime(_ mealTime: String) -> MealTime  {
+        switch mealTime {
+        case "0":
+            return .breakfast
+        case"1":
+            return .lunch
+        case"2":
+            return .dinner
+        default:
+            return .allDay
+        }
+    }
+
+    func getMealType(_ mealType: String) -> MealType {
+        switch mealType {
+        case "중식(특식)":
+            return .special
+        default:
+            return .normal
+        }
+    }
+
+    func getCafeteria(_ cafeteria: String) -> Cafeteria {
+        switch cafeteria {
+        case "생활관식당(블루미르308관)":
+            return .blueMirA
+        case "참슬기식당(310관 B4층)":
+            return .chamseulgi
+        case "생활관식당(블루미르309관)":
+            return .blueMirB
+        case "학생식당(303관B1층)":
+            return .student
+        case "교직원식당(303관B1층)":
+            return .staff
+        case "카우잇츠(cau eats)":
+            return .cauEats
+        default:
+            return .cauEats
+        }
+    }
     
+    func getPrice(_ price: String) -> String {
+        return price.components(separatedBy: [","," ","원"]).joined()
+        
+    }
+
+    func getMenu(_ menu: String) -> [String] {
+        return menu.components(separatedBy: "|")
+    }
 }
