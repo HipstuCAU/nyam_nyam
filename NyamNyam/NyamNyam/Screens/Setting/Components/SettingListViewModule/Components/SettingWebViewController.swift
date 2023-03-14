6826ba157397d28783b2daf8d419b36b1da15f77
Taekhwan Han <hth3396@gmail.com>
2023-03-14 20:16:37 +0900

    feat: #38 WebView에 custom BackButton 적용

--- NyamNyam/NyamNyam/Screens/Setting/Components/SettingListViewModule/Components/SettingWebViewController.swift
+++ NyamNyam/NyamNyam/Screens/Setting/Components/SettingListViewModule/Components/SettingWebViewController.swift
@@ -12,19 +12,37 @@ import WebKit
 final class SettingWebViewController: UIViewController {
     var webView: WKWebView!
     var webURL: String = ""
-
+    
+    lazy var backButton = BackButtonView()
+    
+    @objc func backButtonPressed(_ sender: UIButton) {
+        self.navigationController?.popViewController(animated: true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
+        setBackButtonLayout()
+        backButton.backButton.addTarget(self, action: #selector(backButtonPressed), for: .touchUpInside)
         setWebView()
         setWebViewLayout()
+        view.insetsLayoutMarginsFromSafeArea = true
     }
     
     override func viewDidAppear(_ animated: Bool) {
-           super.viewDidAppear(animated)
+        super.viewDidAppear(animated)
     }
 }
 
 private extension SettingWebViewController {
+    
+    func setBackButtonLayout() {
+        view.addSubview(backButton)
+        backButton.snp.makeConstraints { make in
+            make.top.leading.trailing.equalToSuperview()
+        }
+    }
+    
     func setWebView() {
         let webConfiguration = WKWebViewConfiguration()
         webView = WKWebView(frame: .zero, configuration: webConfiguration)
@@ -38,7 +56,8 @@ private extension SettingWebViewController {
     func setWebViewLayout() {
         view.addSubview(webView)
         webView.snp.makeConstraints { make in
-            make.top.leading.trailing.bottom.equalToSuperview()
+            make.top.equalTo(backButton.snp.bottom).offset(10)
+            make.leading.trailing.bottom.equalToSuperview()
         }
     }
 }
