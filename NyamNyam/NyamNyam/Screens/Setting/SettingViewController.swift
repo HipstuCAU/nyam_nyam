c450aecbec0f34486a47fed5f1b7f7a701a2afce
Taekhwan Han <hth3396@gmail.com>
2023-03-14 21:08:28 +0900

    feat: #38 SetCafeteriaOrderModule 높이를 캠퍼스에 따라서 변경

--- NyamNyam/NyamNyam/Screens/Setting/SettingViewController.swift
+++ NyamNyam/NyamNyam/Screens/Setting/SettingViewController.swift
@@ -44,7 +44,6 @@ final class SettingViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setNavigationBarBackButton()
         setDefaultNavigationBar()
         self.setBackButtonLayout()
         backButton.backButton.addTarget(self, action: #selector(backButtonPressed), for: .touchUpInside)
@@ -60,13 +59,24 @@ final class SettingViewController: UIViewController {
         viewModel.currentCampus.observe(on: self) { [weak self] _ in
             self?.setCampusLabelText()
             self?.resetSetCafeteriaOrderModule()
+            self?.resetSettingListModule()
         }
     }
     
     private func resetSetCafeteriaOrderModule() {
+        setCafeteriaOrderModule.willMove(toParent: nil)
+        setCafeteriaOrderModule.view.removeFromSuperview()
         setCafeteriaOrderModule.removeFromParent()
         setCafeteriaOrderModule = SetCafeteriaOrderTableViewController(viewModel: viewModel)
         setCafeteriaOrderLayout()
+    }
+    
+    private func resetSettingListModule() {
+        settingListModule.willMove(toParent: nil)
+        settingListModule.view.removeFromSuperview()
+        settingListModule.removeFromParent()
+        settingListModule = SettingListTableViewController()
+        setSettingListLayout()
     }
     
     private func setDefaultNavigationBar() {
@@ -96,7 +106,7 @@ private extension SettingViewController {
         setDefaultCampusModule.snp.makeConstraints { make in
             make.top.equalTo(backButton.snp.bottom)
             make.leading.trailing.equalToSuperview()
-            make.height.equalTo(50)
+            make.height.equalTo(55)
         }
     }
     
@@ -107,7 +117,7 @@ private extension SettingViewController {
         setCafeteriaOrderModule.view.snp.makeConstraints { make in
             make.top.equalTo(setDefaultCampusModule.snp.bottom).offset(16)
             make.leading.trailing.equalToSuperview()
-            make.height.equalTo(270)
+            make.height.equalTo(viewModel.currentCampus.value == .seoul ?  UserDefaults.standard.seoulCafeteria.count * 40 + 58 : UserDefaults.standard.ansungCafeteria.count * 40 + 58)
         }
     }
     
@@ -118,7 +128,6 @@ private extension SettingViewController {
         settingListModule.view.snp.makeConstraints { make in
             make.top.equalTo(setCafeteriaOrderModule.view.snp.bottom).offset(16)
             make.leading.trailing.bottom.equalToSuperview()
-            
         }
     }
     
