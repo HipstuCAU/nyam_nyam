2d76db99e39ca18a56b62fb64614cd39ee5ff1d0
Taekhwan Han <hth3396@gmail.com>
2023-03-16 17:32:23 +0900

    feat: #57 네트워크 미연결시 WebView 예외처리 Alert와 Reachability를 사용하여 네트워크가 연결되어있지 않을 시 WebView를 예외처리 하였습니다.

--- NyamNyam/NyamNyam/Screens/Setting/Components/SettingListViewModule/Components/SettingWebViewController.swift
+++ NyamNyam/NyamNyam/Screens/Setting/Components/SettingListViewModule/Components/SettingWebViewController.swift
@@ -20,7 +20,17 @@ final class SettingWebViewController: UIViewController {
     }
     
     override func viewDidAppear(_ animated: Bool) {
-           super.viewDidAppear(animated)
+        super.viewDidAppear(animated)
+        
+        guard Reachability.networkConnected() else {
+            let alert = UIAlertController(title: "네트워크가 원활하지 않습니다", message: "연결 상태를 확인해주세요", preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "닫기", style: .default) {  _ in
+                self.navigationController?.popViewController(animated: true)
+            }
+            alert.addAction(okAction)
+            self.present(alert, animated: true, completion: nil)
+            return
+        }
     }
 }
 
