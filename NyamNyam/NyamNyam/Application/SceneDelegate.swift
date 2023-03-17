e9712ce724f9fb8e3830d38194544b7624967902
Taekhwan Han <hth3396@gmail.com>
2023-03-17 14:12:05 +0900

    feat: #52 네트워크 검사 후 내려받도록 구현

--- NyamNyam/NyamNyam/Application/SceneDelegate.swift
+++ NyamNyam/NyamNyam/Application/SceneDelegate.swift
@@ -14,12 +14,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        let viewController = HomeViewController()
-        let navigationController = UINavigationController(rootViewController: viewController)
-        
-        window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = navigationController
-        window?.makeKeyAndVisible()
+            let viewController = HomeViewController()
+            let navigationController = UINavigationController(rootViewController: viewController)
+            
+            self.window = UIWindow(windowScene: windowScene)
+            self.window?.rootViewController = navigationController
+            self.window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
@@ -43,12 +43,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Called as the scene transitions from the background to the foreground.
         // Use this method to undo the changes made on entering the background.
         
-        //MARK: 임시 테스트 구문 추후 FireStore 업로드 로직 변경예정
+        //MARK: 네트워크 검사 후 FireStore 내려받기
         if Reachability.networkConnected() {
+            FBManager.shared.getMealJson()
             UserDefaults.standard.lastUploadDate = Date().toFullTimeString()
-            print(UserDefaults.standard.lastUploadDate)
         } else {
-            print("네트워크 연결 오류")
+            //TODO: 테스트구문 추후 UI 이벤트 처리 예정
+            print("네트워크 연결안됨")
         }
     }
 
