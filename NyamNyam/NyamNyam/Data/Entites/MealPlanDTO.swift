57c904c82348c9b84bb4c0b36c691b681fdfdb8b
Taekhwan Han <hth3396@gmail.com>
2024-01-10 11:45:14 +0900

    refactor: MealPlanDTO nullable 제거, CodingKeys로 default 처리

--- NyamNyam/NyamNyam/Data/Entites/MealPlanDTO.swift
+++ NyamNyam/NyamNyam/Data/Entites/MealPlanDTO.swift
@@ -17,15 +17,38 @@ struct MenuDTO: Codable {
 struct CafeteriaDTO: Codable {
     let name: String
     let menu: [MenuDTO]
+    
+    init() {
+        self.name = ""
+        self.menu = []
+    }
 }
 
 struct DailyMenuDTO: Codable {
     let date: String
-    let breakfast: CafeteriaDTO?
-    let lunch: CafeteriaDTO?
-    let dinner: CafeteriaDTO?
-    let all: CafeteriaDTO?
+    var breakfast: CafeteriaDTO
+    var lunch: CafeteriaDTO
+    var dinner: CafeteriaDTO
+    var all: CafeteriaDTO
+
+    enum CodingKeys: String, CodingKey {
+        case date
+        case breakfast
+        case lunch
+        case dinner
+        case all
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        date = try container.decode(String.self, forKey: .date)
+        breakfast = try container.decodeIfPresent(CafeteriaDTO.self, forKey: .breakfast) ?? CafeteriaDTO()
+        lunch = try container.decodeIfPresent(CafeteriaDTO.self, forKey: .lunch) ?? CafeteriaDTO()
+        dinner = try container.decodeIfPresent(CafeteriaDTO.self, forKey: .dinner) ?? CafeteriaDTO()
+        all = try container.decodeIfPresent(CafeteriaDTO.self, forKey: .all) ?? CafeteriaDTO()
+    }
 }
+
 
 struct WeeklyMenuDTO: Codable {
     let campus: String
