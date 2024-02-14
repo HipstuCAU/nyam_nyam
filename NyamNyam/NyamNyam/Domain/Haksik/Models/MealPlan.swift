d8c3ebb05c465ca09646407d96c66c8ec8853a5a
Taekhwan Han <hth3396@gmail.com>
2024-02-14 17:08:33 +0900

    feat: #105 MealPlan 구성

--- NyamNyam/NyamNyam/Domain/Haksik/Models/MealPlan.swift
+++ NyamNyam/NyamNyam/Domain/Haksik/Models/MealPlan.swift
@@ -7,6 +7,29 @@
 
 import Foundation
 
-struct MealPlan: Equatable {
-    
+struct MealPlan {
+    let date: Date
+    let cafeterias: [Cafeteria]
+}
+
+struct Cafeteria {
+    let cafeteriaID: String
+    let meals: [MealType]
+}
+
+struct MealType {
+    let mealType: String
+    let shouldShowTime: Bool
+    let startTime: Date
+    let endTime: Date
+    let menus: [Menu]
+}
+
+struct Menu {
+    let menuType: String
+    let price: Int
+    let startTime: Date
+    let endTime: Date
+    let menu: [String]
+    let calories: Double
 }
